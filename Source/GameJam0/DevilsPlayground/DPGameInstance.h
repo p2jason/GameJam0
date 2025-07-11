11e8acb65b4bc8b9835dda46d9ac4891c2db46cb
QuickJAB <Redman9782@gmail.com>
2025-07-11 23:04:29 +0100

    Game Inst, Game Mode, Game State

---
+++ Source/GameJam0/DevilsPlayground/DPGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "DPGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEJAM0_API UDPGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/GameJam0/DevilsPlayground/DPGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DPGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEJAM0_API ADPGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
