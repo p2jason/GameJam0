2819b400d651123b2908be36ce00d05535dc1cd0
HeyItsCoffee <ryanholtnz@gmail.com>
2025-07-12 13:20:58 +0100

    UI - Base HUD and Widget Class

---
+++ Source/GameJam0/DevilsPlayground/GameFramework/DPHUD.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "DPHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEJAM0_API ADPHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/GameJam0/DevilsPlayground/UI/DPActivatableWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonActivatableWidget.h"
+#include "DPActivatableWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEJAM0_API UDPActivatableWidget : public UCommonActivatableWidget
+{
+	GENERATED_BODY()
+	
+};
