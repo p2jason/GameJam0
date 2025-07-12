3f9d974009831030d66573f19ce0250f3798c564
MarkNicholsonDev <123469179+MarkNicholsonDev@users.noreply.github.com>
2025-07-12 14:18:42 +0100

    Added basic interaction system with an interaction component and additional blockout assets
    Added a skeleton key model and a chest for now

---
+++ Source/GameJam0/DevilsPlayground/DPInteractableComponent.cpp
@@ -0,0 +1,77 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DevilsPlayground/DPInteractableComponent.h"
+
+DEFINE_LOG_CATEGORY_STATIC(LogInteraction, Verbose, All);
+
+// Sets default values for this component's properties
+UDPInteractableComponent::UDPInteractableComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// Create and attach a sphere component
+	InteractionArea = CreateDefaultSubobject<USphereComponent>(TEXT("InteractionArea"));
+	InteractionArea->SetupAttachment(this);
+}
+
+
+// Called when the game starts
+void UDPInteractableComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	InteractionArea->OnComponentBeginOverlap.AddDynamic(this, &ThisClass::OnInteractionAreaBeginOverlap);
+	InteractionArea->OnComponentEndOverlap.AddDynamic(this, &ThisClass::OnInteractionAreaEndOverlap);
+}
+
+
+// Called every frame
+void UDPInteractableComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+
+void UDPInteractableComponent::OnInteractionAreaBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepHitResult)
+{
+	UE_LOG(LogInteraction, Verbose, TEXT("Entered Interaction Area"));
+
+	BeginInteraction(OtherActor);
+}
+
+void UDPInteractableComponent::OnInteractionAreaEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	UE_LOG(LogInteraction, Verbose, TEXT("Left Interaction Area"));
+
+	EndInteraction(OtherActor);
+}
+
+void UDPInteractableComponent::BeginInteraction(AActor* Interactor) 
+{	
+	InteractingActor = Interactor;
+
+	OnInteractionBegin.Broadcast(InteractingActor);
+}
+
+void UDPInteractableComponent::CancelInteraction(AActor* Interactor)
+{
+	OnInteractionCancelled.Broadcast(InteractingActor);
+
+	InteractingActor = nullptr;
+}
+
+void UDPInteractableComponent::EndInteraction(AActor* Interactor)
+{
+	OnInteractionEnd.Broadcast(InteractingActor);
+
+	InteractingActor = nullptr;
+}
+
+
---
+++ Source/GameJam0/DevilsPlayground/DPInteractableComponent.h
@@ -0,0 +1,64 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SphereComponent.h"
+#include "Delegates/DelegateCombinations.h"
+#include "DPInteractableComponent.generated.h"
+
+// Delegates
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnInteractionEvent, AActor*, Interactor);
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class GAMEJAM0_API UDPInteractableComponent : public USceneComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UDPInteractableComponent();
+
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	// Delegate events
+	UPROPERTY(BlueprintAssignable, Category = "Interaction")
+	FOnInteractionEvent OnInteractionBegin;
+
+	UPROPERTY(BlueprintAssignable, Category = "Interaction")
+	FOnInteractionEvent OnInteractionCancelled;
+
+	UPROPERTY(BlueprintAssignable, Category = "Interaction")
+	FOnInteractionEvent OnInteractionEnd;
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(EditAnywhere)
+	USphereComponent* InteractionArea;
+
+	UPROPERTY(EditDefaultsOnly)
+	UStaticMeshComponent* InteractIcon;
+
+	AActor* InteractingActor;
+
+	UFUNCTION()
+	void OnInteractionAreaBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepHitResult);
+
+	UFUNCTION()
+	void OnInteractionAreaEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	UFUNCTION()
+	void BeginInteraction(AActor* Interactor);
+
+	UFUNCTION()
+	void CancelInteraction(AActor* Interactor);
+
+	UFUNCTION()
+	void EndInteraction(AActor* Interactor);
+};
